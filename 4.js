45ca2651912f2ae19ce755ab9495b277c40bbdf9
vanhopld98 <vanhopld98@gmail.com>
2021-07-08 14:48:15 +0700

    bai 4

---
+++ 4.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều dài")
+inputHeight = prompt("Nhập chiều cao")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is:" + area);
